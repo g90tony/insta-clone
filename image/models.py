961c55345ef793c606628123ed6e94972b9bb84e
Caleb Tony <calebmbugua@gmail.com>
2021-05-25 17:09:08 +0300

    added follower following and joined attributes to profile and created_at to image

--- image/models.py
+++ image/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-from django.db.models.base import Model
+from django.contrib.postgres.fields import ArrayField
+from django.db.models.deletion import CASCADE
+
 # Create your models here.
 
 class Profile(models.Model):
     profile_photo = CloudinaryField('image', default = None)
     bio = models.TextField()
-    user_name = models.CharField()
+    user_name = models.CharField(max_length=255)  
+    followers = ArrayField(models.IntegerField())
+    following = ArrayField(models.IntegerField())
+    joined = models.DateField(auto_now_add=True)
+    
     
     def save_profile(self):
         self.save()
@@ -17,16 +23,20 @@ class Profile(models.Model):
     def update_bio(self, profile_id, update_profile_bio): 
         self.objects(id = profile_id).update(bio = update_profile_bio)
         
-    def delete_profile(self, profile_id):
+    def delete_profile(self):
         self.objects.delete()
+    
+    def __str__(self):
+        return self.user_name
         
 
 class Image(models.Model):
     image = CloudinaryField('image', default= None)
-    image_name = models.CharField()
-    image_caption = models.CharField()
-    profile_id = models.ForeignKey(Profile)
+    image_name = models.CharField(max_length=255)
+    image_caption = models.CharField(max_length=255)
+    profile_id = models.ForeignKey(Profile , on_delete=CASCADE)
     likes = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
     
     def save_image(self):
         self.save()
@@ -36,3 +46,6 @@ class Image(models.Model):
         
     def update_caption(self, image_id, new_caption):
         self.objects.filter(id = image_id).update(image_caption = new_caption)
+        
+    def __str__(self):
+        return self.name
