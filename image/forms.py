196cb34d1dc1f5d018604b054cd3fa6d6f1573a8
Caleb Tony <calebmbugua@gmail.com>
2021-05-26 17:56:07 +0300

    did major editing to forms

--- image/forms.py
+++ image/forms.py
@@ -1,8 +1,22 @@
+from django.db.models import fields
+from .models import Profile, Image
 from django import forms
 from cloudinary.models import CloudinaryField
-from django.db.models.fields import CharField, TextField
+from django.forms.widgets import Textarea
+
 
 class NewImageForm(forms.Form):
-    new_image = CloudinaryField('image', default=None)
-    image_caption = TextField()
-    image_name = CharField()
+    new_image = forms.FileField()
+    image_caption = forms.CharField( max_length=255, required=False, widget=Textarea)
+    
+    class Meta:
+        model = Image
+        fields = ('new_image','image_caption')
+    
+class NewProfileForm(forms.Form):
+    profile_photo = forms.FileField()
+    bio = forms.CharField( max_length=255, required=False, widget=Textarea)
+    
+    class Meta:
+        model = Profile
+        fields = ('profile_photo', 'bio')
