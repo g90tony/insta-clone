c86daec428e166a4f08b963c9c70e4ad665f7f18
Caleb Tony <calebmbugua@gmail.com>
2021-05-26 17:56:51 +0300

    added all necessary routes

--- image/urls.py
+++ image/urls.py
@@ -4,4 +4,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='Dashboard'),
+    path('upload/', views.upload, name='Upload'),
+    path('profile/', views.profile, name='Profile'),
+    path('profile/create', views.create_profile, name='Create Profile'),
+    path('profile/view/<int:user_id>/', views.view_profile, name='View Profile'),
+    path('post/view/<int:post_id>/', views.view, name='View Post'),
+    path('profile/view/search', views.search, name='Search'),
 ]
